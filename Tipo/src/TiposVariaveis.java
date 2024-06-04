7609bf4bb0dcc68fe86801f5d33a0f1ae2e3bf63
Yasmim Soares <yasmimsousa251@gmail.com>
2024-06-04 12:16:07 -0300

    regras

--- Tipo/src/TiposVariaveis.java
+++ Tipo/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //Java Beans - metodos
+        // JAva sintaxe - PRIMITIVOS - valores reservados
+        //Constates - final double CAIXA_ALTA = 15.33;
         System.out.println("Hello, World!");
     }
 }
