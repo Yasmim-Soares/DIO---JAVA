7f617480d6fbd02f24f5c6eb062cac08c9d4ac00
Yasmim Soares <yasmimsousa251@gmail.com>
2024-06-04 17:05:05 -0300

    Novas regras de sintaxe

---
+++ Tipo/src/Sintaxe/sintaxe/PalavrasReservadas.java
@@ -0,0 +1,7 @@
+package Sintaxe.sintaxe;
+
+public class PalavrasReservadas {
+//Palavras não podem ser reutilizadas
+//Palavras que fazem parte do java não podem ser utilizadas
+//Se acostumar com o as palavras de controle de fluxo que está no material de apoio
+}
